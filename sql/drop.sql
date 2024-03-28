571e3a68a1557b642ae3090b3ba8aacac5853dd0
OmkarBhat16 <omkar.16.bhat@gmail.com>
2024-03-28 19:45:33 +0530

    sql files initalized

---
+++ sql/drop.sql
@@ -0,0 +1,11 @@
+-- Drop tables
+DROP TABLE IF EXISTS experience;
+DROP TABLE IF EXISTS skills;
+DROP TABLE IF EXISTS applications;
+DROP TABLE IF EXISTS projects;
+DROP TABLE IF EXISTS users;
+
+-- Drop types
+DROP TYPE IF EXISTS application_status;
+DROP TYPE IF EXISTS project_status;
+DROP TYPE IF EXISTS user_type;
