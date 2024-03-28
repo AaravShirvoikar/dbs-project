d610b22b6d29fae3f7eeb2be9605fdf11c4f2c96
AaravShirvoikar <aaravshirvoikar685@gmail.com>
2024-03-28 23:28:03 +0530

    added cors middleware

---
+++ backend/middleware/cors.go
@@ -0,0 +1,20 @@
+package middleware
+
+import (
+	"net/http"
+)
+
+func Cors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusNoContent)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
+}
