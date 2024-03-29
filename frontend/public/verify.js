c5e34d6956575f041b0f5e71908d2fa667021679
OmkarBhat16 <omkar.16.bhat@gmail.com>
2024-03-30 03:35:43 +0530

    logout implemented

--- frontend/public/verify.js
+++ frontend/public/verify.js
@@ -2,3 +2,8 @@ if(localStorage.getItem("token") == null){
     location.replace("../index.html");
     alert("You are not logged in. Please login to continue");
 }
+
+function logout(){
+    localStorage.clear();
+    location.replace("../index.html");
+}
