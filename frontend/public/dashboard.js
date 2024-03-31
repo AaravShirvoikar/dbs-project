fda67964ad1c7d8780f3a7e56dc0a8715c710e61
OmkarBhat16 <omkar.16.bhat@gmail.com>
2024-04-01 02:09:57 +0530

    added bookmark button and partially  added dashboard details

--- frontend/public/dashboard.js
+++ frontend/public/dashboard.js
@@ -1,14 +1,8 @@
-document.getElementById("welcome-text").innerHTML = "Welcome "+localStorage.getItem("username");
-let profileDetails = document.createElement("div");
-profileDetails.classList.add("profile-details");
-let userName = document.createElement("h4");
-userName.innerHTML = "Username: "+localStorage.getItem("username");
-profileDetails.appendChild(userName);
-document.getElementById("profile").appendChild(profileDetails);
 
 
 
-async function fetchData() {
+
+async function fetchData1() {
     let headersList = {
         "Accept": "*/*",
         "Content-Type": "application/json",
@@ -34,22 +28,60 @@ async function fetchData() {
     return data;
 }
 
-async function getData() {
-    let response = await fetchData();
+async function fetchData2() {
+    let headersList = {
+        "Accept": "*/*",
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + localStorage.getItem("token"),
+    }
+    let response;
+    try {
+        response = await fetch("http://localhost:8080/user/", { 
+            method: "GET",
+            headers: headersList
+        });
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        return;
+    }
 
-    console.log(response);
-    
-    for (let i in response) {
-    let project = response[i];
-    // console.log(project);
-    let projectCard = document.createElement("div");
-    projectCard.className = "project-entry";
-    let projectName = document.createElement("li");
-    // console.log(project.title);
-    projectName.innerHTML = project.title;
-    projectCard.appendChild(projectName);
-    document.getElementById("project-list").appendChild(projectCard);
+    let data = JSON.parse(await response.text());
+    console.log(data);   
+    if(data.error == "invalid token format" || data.error == "invalid token"){
+        alert("Session Expired. Please login again.");
+        window.location.href = "./login.html";
+    }
+    return data;
 }
+
+async function getData() {
+    let response = await fetchData1();
+    console.log(response);
+    let response2 = await fetchData2();
+    console.log(response2);
+    
+    document.getElementById("welcome-text").innerHTML = "Welcome "+localStorage.getItem("username");
+    let profileDetails = document.createElement("div");
+    profileDetails.classList.add("profile-details");
+    let userName = document.createElement("h4");
+    userName.innerHTML = "Username: "+response2.username;
+    let emailId = document.createElement("h4");
+    emailId.innerHTML = "Email: "+response2.email;
+    profileDetails.appendChild(userName);
+    profileDetails.appendChild(emailId);
+    document.getElementById("profile").appendChild(profileDetails);
+
+    for (let i in response) {
+        let project = response[i];
+        // console.log(project);
+        let projectCard = document.createElement("div");
+        projectCard.className = "project-entry";
+        let projectName = document.createElement("li");     
+        // console.log(project.title);
+        projectName.innerHTML = project.title;
+        projectCard.appendChild(projectName);
+        document.getElementById("project-list").appendChild(projectCard);
+    }
     console.log("Display Complete");
 };
 getData();
