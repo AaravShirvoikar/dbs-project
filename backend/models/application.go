f04428d55b765a411328881d74cfe6712dc0ebce
AaravShirvoikar <aaravshirvoikar685@gmail.com>
2024-04-01 10:38:56 +0530

    now really

---
+++ backend/models/application.go
@@ -0,0 +1,59 @@
+package models
+
+import (
+	"github.com/AaravShirvoikar/dbs-project/backend/database"
+)
+
+type Application struct {
+	ApplicationID int    `json:"application_id"`
+	StudentID     int    `json:"student_id"`
+	ProjectID     int    `json:"project_id"`
+	Message       string `json:"message"`
+	Status        string `json:"status"`
+}
+
+func (a *Application) Create() error {
+	_, err := database.Db.Exec("INSERT INTO applications (student_id, project_id, message, status) VALUES ($1, $2, $3, $4)", a.StudentID, a.ProjectID, a.Message, a.Status)
+	return err
+}
+
+func GetStudentApplications(id int) ([]Application, error) {
+	query := "SELECT id, message, status FROM applications WHERE student_id = $1"
+	rows, err := database.Db.Query(query, id)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	applications := []Application{}
+	for rows.Next() {
+		var application Application
+		err := rows.Scan(&application.ApplicationID, &application.Message, &application.Status)
+		if err != nil {
+			return nil, err
+		}
+		applications = append(applications, application)
+	}
+
+	return applications, nil
+}
+
+func GetProfessorApplications(id int) ([]Application, error) {
+	rows, err := database.Db.Query("SELECT applications.id, message, applications.status FROM applications JOIN projects ON applications.project_id = projects.id WHERE projects.professor_id = $1", id)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	applications := []Application{}
+	for rows.Next() {
+		var application Application
+		err := rows.Scan(&application.ApplicationID, &application.Message, &application.Status)
+		if err != nil {
+			return nil, err
+		}
+		applications = append(applications, application)
+	}
+
+	return applications, nil
+}
